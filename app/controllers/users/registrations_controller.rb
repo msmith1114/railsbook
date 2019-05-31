0ef75d7d40aab2bf7de84450cdaa9500c3c92655
Matt Smith <msmith@missiondata.com>
2019-05-31 00:32:29 -0400

    Facebook omniauth edit profile password fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,7 +40,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  # Overwrite update_resource to let users to update their user without giving their password
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   #def configure_sign_up_params
